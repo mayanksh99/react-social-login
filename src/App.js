cc2d8f66441e9d87b021ef9466488ddc2050eff5
mayanksh99 <mayankshakya992@gmail.com>
2020-01-25 18:28:05 +0530

    add google login

--- src/App.js
+++ src/App.js
@@ -1,56 +1,72 @@
 import React, { useState } from "react";
 import "./App.css";
-import SocialButton from "./SocialButton";
+// import SocialButton from "./SocialButton";
+import FacebookLogin from "react-facebook-login";
+import { GoogleLogin } from "react-google-login";
+import { LinkedIn } from "react-linkedin-login-oauth2";
 
 function App() {
   const [state, setState] = useState({ isloggedIn: false, user: "" });
-  const handleSocialLogin = user => {
-    console.log("user", user._profile);
-    setState({ isloggedIn: true, user: user._profile });
-  };
+  // const handleSocialLogin = user => {
+  //   console.log("user", user._profile);
+  //   setState({ isloggedIn: true, user: user._profile });
+  // };
 
-  const handleSocialLoginFailure = err => {
+  const handleFailure = err => {
     console.error(err);
   };
 
   const handleLogout = () => {
     setState({ isloggedIn: false, user: "" });
   };
+  const responseFacebook = response => {
+    console.log(response);
+    setState({ isloggedIn: true, user: response });
+  };
+  const responseGoogle = response => {
+    console.log(response.profileObj);
+    setState({ isloggedIn: true, user: response.profileObj });
+  };
   return (
     <>
       <div className="container">
         <h1 className="heading">Social Login</h1>
+
+        {!state.isloggedIn ? (
+          <div>
+            <FacebookLogin
+              appId="1763913083744541"
+              autoLoad={true}
+              fields="name,email,picture"
+              callback={responseFacebook}
+            />
+            <br></br>
+            <GoogleLogin
+              clientId="805194883716-hqmtmdffp68a10tl323nd3eplrblnb9h.apps.googleusercontent.com"
+              buttonText="Login with Google"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
+        ) : (
+          <div>
+            <h1>{state.user.name}</h1>
+            <img
+              className="img-fluid"
+              src={state.user.picture.data.url || state.user.imageUrl}
+              height="200px"
+              width="200px"
+              alt=""
+            ></img>
+            <br></br>
+            <button className="btn btn-primary" onClick={handleLogout}>
+              Log Out
+            </button>
+          </div>
+        )}
       </div>
-      {!state.isloggedIn ? (
-        <div>
-          <SocialButton
-            provider="facebook"
-            appId="1763913083744541"
-            onLoginSuccess={handleSocialLogin}
-            onLoginFailure={handleSocialLoginFailure}
-          >
-            Login with Facebook
-          </SocialButton>
-        </div>
-      ) : (
-        <div>
-          <h1>{state.user.name}</h1>
-          <button onClick={handleLogout}>Log Out</button>
-        </div>
-      )}
     </>
-    // if(state.isloggedIn){
-    //   <div></div>
-    // }else{
-    //   <SocialButton
-    //   provider='facebook'
-    //   appId='YOUR_APP_ID'
-    //   onLoginSuccess={handleSocialLogin}
-    //   onLoginFailure={handleSocialLoginFailure}
-    // >
-    //   Login with Facebook
-    // </SocialButton>
-    // }
   );
 }
 
